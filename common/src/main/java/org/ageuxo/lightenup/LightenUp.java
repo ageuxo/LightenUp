1a05c66e16748d8ca3f5cd8ddfa560b99f370b9d
ageuxo <ageuxo@gmail.com>
2025-03-09 12:10:36 +0100

    Add light level Add axe scraping to lower light level

--- common/src/main/java/org/ageuxo/lightenup/LightenUp.java
+++ common/src/main/java/org/ageuxo/lightenup/LightenUp.java
@@ -5,7 +5,6 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.ageuxo.lightenup.block.GlowPasteBlock;
-import org.ageuxo.lightenup.item.GlowPasteItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +18,7 @@ public class LightenUp {
     private static final String MOD_NAME = "Lighten Up";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
-    public static final Supplier<GlowPasteBlock> GLOW_PASTE_BLOCK = PLATFORM.registerBlock("glow_paste", ()-> new GlowPasteBlock(BlockBehaviour.Properties.of().noOcclusion()));
+    public static final Supplier<GlowPasteBlock> GLOW_PASTE_BLOCK = PLATFORM.registerBlock("glow_paste", ()-> new GlowPasteBlock(BlockBehaviour.Properties.of().noOcclusion().lightLevel(GlowPasteBlock::stateToLightLevel)));
 
     public static final Supplier<BlockItem> GLOW_PASTE_ITEM = PLATFORM.registerItem("glow_paste", ()-> new BlockItem(GLOW_PASTE_BLOCK.get(), new Item.Properties()));
 
--- common/src/main/java/org/ageuxo/lightenup/block/GlowPasteBlock.java
+++ common/src/main/java/org/ageuxo/lightenup/block/GlowPasteBlock.java
@@ -1,10 +1,22 @@
 package org.ageuxo.lightenup.block;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -13,20 +25,46 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class GlowPasteBlock extends MultifaceBlock {
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    public static final BooleanProperty UP = BlockStateProperties.UP;
-    public static final BooleanProperty DOWN = BlockStateProperties.DOWN;
-    public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
-    public static final BooleanProperty EAST = BlockStateProperties.EAST;
-    public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
-    public static final BooleanProperty WEST = BlockStateProperties.WEST;
-    public static final IntegerProperty LIGHT = IntegerProperty.create("light", 1, 15);
+    public static final IntegerProperty LIGHT = IntegerProperty.create("light", 1, 3);
 
     public GlowPasteBlock(Properties properties) {
         super(properties);
+        registerDefaultState(defaultBlockState().setValue(LIGHT, 3));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(WATERLOGGED)
+                .add(LIGHT);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        ItemStack stack = player.getItemInHand(hand);
+        int lightLevel = state.getValue(LIGHT);
+        if (stack.is(ItemTags.AXES)) {
+            level.playSound(player, pos, SoundEvents.AXE_SCRAPE, SoundSource.BLOCKS, 1.0F, 1.0F);
+            level.levelEvent(player, LevelEvent.PARTICLES_SCRAPE, pos, 0);
+            stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand));
+            if (lightLevel > 1) {
+                level.setBlockAndUpdate(pos, state.setValue(LIGHT, lightLevel - 1));
+            } else {
+                level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+            }
+            return InteractionResult.SUCCESS;
+        }
+
+        return super.use(state, level, pos, player, hand, hit);
     }
 
     @Override
     public MultifaceSpreader getSpreader() {
         return new MultifaceSpreader(this);
+    }
+
+    public static int stateToLightLevel(BlockState state) {
+        return state.getValue(LIGHT) * 5;
     }
 }

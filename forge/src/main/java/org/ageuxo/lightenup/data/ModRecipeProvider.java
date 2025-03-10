2e972b1da8f84e77abdc11b9b1d2837a351454c0
ageuxo <ageuxo@gmail.com>
2025-03-10 15:50:25 +0100

    Add transient paste recipe

--- forge/src/main/java/org/ageuxo/lightenup/data/ModRecipeProvider.java
+++ forge/src/main/java/org/ageuxo/lightenup/data/ModRecipeProvider.java
@@ -3,7 +3,11 @@ package org.ageuxo.lightenup.data;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import org.ageuxo.lightenup.LightenUp;
 
@@ -21,5 +25,18 @@ public class ModRecipeProvider extends RecipeProvider {
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> writer) {
         smeltingResultFromBase(writer, LightenUp.GLOW_PASTE_ITEM.get(), Items.GLOW_BERRIES);
+
+        surroundedIngredient(writer, LightenUp.TRANSIENT_PASTE_ITEM.get(), 16, Items.ENDER_PEARL, LightenUp.GLOW_PASTE_ITEM.get());
+    }
+
+    private static void surroundedIngredient(Consumer<FinishedRecipe> writer, BlockItem result, int count, Item centerIngredient, BlockItem outerIngredient) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, result, count)
+                .define('O', outerIngredient)
+                .define('X', centerIngredient)
+                .pattern("OOO")
+                .pattern("OXO")
+                .pattern("OOO")
+                .unlockedBy(getHasName(outerIngredient), has(outerIngredient))
+                .save(writer);
     }
 }
